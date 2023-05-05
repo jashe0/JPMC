871174ef1b3eaf2036e616fa72ce7947803a3c1a
jashe0 <120477825+jashe0@users.noreply.github.com>
2023-05-05 13:46:45 +0100

    looking further at arraylists

--- ArrayLIsts/src/Main.java
+++ ArrayLIsts/src/Main.java
@@ -1,9 +1,13 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 
 record GroceryItem(String name, String type, int count){
     public GroceryItem(String name) {
         this(name, "DAIRY", 1);
+    }
+
+    @Override
+    public String toString(){
+        return String.format("%d %s in %s",count,name.toUpperCase(),type);
     }
 }
 
@@ -13,7 +17,7 @@ public class Main {
         groceryArray[0] = new GroceryItem("milk");
         groceryArray[1] = new GroceryItem("apples","PRODUCE",6);
         groceryArray[2] = new GroceryItem("oranges","PRODUCE",5);
-        System.out.println(Arrays.toString(groceryArray));
+        //System.out.println(Arrays.toString(groceryArray));
 
         ArrayList objectList = new ArrayList();
         objectList.add(new GroceryItem("Butter"));
@@ -21,5 +25,11 @@ public class Main {
 
         ArrayList <GroceryItem> groceryList = new ArrayList<>();
         groceryList.add(new GroceryItem("Butter"));
+        groceryList.add(new GroceryItem("milk"));
+        groceryList.add(new GroceryItem("oranges","PRODUCE",5));
+        groceryList.set(0, new GroceryItem("apples","PRODUCE",6));
+        System.out.println(groceryList);
+        groceryList.remove(1);
+        System.out.println(groceryList);
     }
 }
