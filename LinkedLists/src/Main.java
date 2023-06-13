365d76a8e150bdb532f410f2de53e63d302d0ec9
jashe0 <120477825+jashe0@users.noreply.github.com>
2023-06-13 15:17:20 +0100

    looking further at list iterator

--- LinkedLists/src/Main.java
+++ LinkedLists/src/Main.java
@@ -19,7 +19,9 @@ public class Main {
         //gettingElements(placesToVisit);
         //printItinerary(placesToVisit);
         //printItinerary2(placesToVisit);
-        printItinerary3(placesToVisit);
+        //printItinerary3(placesToVisit);
+
+        testIterator(placesToVisit);
     }
 
     private static void addMoreElements(LinkedList<String> list){
@@ -111,5 +113,22 @@ public class Main {
             previousTown = town;
         }
         System.out.println("Trip ends at " + list.getLast());
+    }
+
+    private static void testIterator(LinkedList<String> list){
+        var iterator = list.listIterator();
+        while (iterator.hasNext()){
+            //System.out.println(iterator.next());
+            if(iterator.next().equalsIgnoreCase("Brisbane")){
+                iterator.add("Lake Wivenhoe");
+            }
+        }
+        while (iterator.hasPrevious()){
+            System.out.println(iterator.previous());
+        }
+        System.out.println(list);
+
+        var iterator2 = list.listIterator(3);
+        System.out.println(iterator2.previous());
     }
 }
