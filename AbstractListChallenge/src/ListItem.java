5907ab2ba9f4525a93a73905349e1443b98ef1ad
jashe0 <120477825+jashe0@users.noreply.github.com>
2023-06-24 15:45:00 +0100

    complete abstract list challenge

--- AbstractListChallenge/src/ListItem.java
+++ AbstractListChallenge/src/ListItem.java
@@ -7,7 +7,21 @@ public abstract class ListItem {
         this.value = value;
     }
 
-    private abstract ();
+    abstract ListItem next() ;
 
-    // write code here
+    abstract ListItem setNext(ListItem item);
+
+    abstract ListItem previous();
+
+    abstract ListItem setPrevious(ListItem item);
+
+    abstract int compareTo(ListItem item);
+
+    public Object getValue() {
+        return value;
+    }
+
+    public void setValue(Object value) {
+        this.value = value;
+    }
 }
--- AbstractListChallenge/src/NodeList.java
+++ AbstractListChallenge/src/NodeList.java
@@ -1,2 +1,8 @@
-public class NodeList {
+public interface NodeList {
+    ListItem getRoot();
+    boolean addItem(ListItem item);
+
+    boolean removeItem(ListItem item);
+
+    void traverse(ListItem root);
 }
