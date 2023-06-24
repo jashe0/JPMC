2471f9e4c7417853dbb694af21ecccd8549cfa55
jashe0 <120477825+jashe0@users.noreply.github.com>
2023-06-24 14:13:10 +0100

    complete challenge using abstract

---
+++ AbstractListChallenge/src/ListItem.java
@@ -0,0 +1,13 @@
+public abstract class ListItem {
+    protected ListItem rightLink;
+    protected ListItem leftLink;
+    protected Object value;
+
+    public ListItem(Object value) {
+        this.value = value;
+    }
+
+    private abstract ();
+
+    // write code here
+}
