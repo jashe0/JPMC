e3cae830870a8559c1f1ea63e6158201b2acb898
jashe0 <120477825+jashe0@users.noreply.github.com>
2023-06-25 15:09:32 +0100

    Looking at interfaces

---
+++ Interfaces/src/Dragonfly.java
@@ -0,0 +1,16 @@
+public record Dragonfly(String name, String type) implements FlightEnabled {
+    @Override
+    public void takeOff() {
+
+    }
+
+    @Override
+    public void land() {
+
+    }
+
+    @Override
+    public void fly() {
+
+    }
+}
---
+++ Interfaces/src/Satellite.java
@@ -0,0 +1,22 @@
+public class Satellite implements OrbitEarth {
+
+    @Override
+    public void takeOff() {
+
+    }
+
+    @Override
+    public void land() {
+
+    }
+
+    @Override
+    public void fly() {
+
+    }
+
+    @Override
+    public void achieveOrbit() {
+        System.out.println("Orbit achieved!");
+    }
+}
