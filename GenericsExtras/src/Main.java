1a58bad0e096681e7c9dcb1e51dfe7862cffabee
jashe0 <120477825+jashe0@users.noreply.github.com>
2023-07-20 15:28:03 +0100

    looking at generic methods and wildcards as well as type erasure

---
+++ GenericsExtras/src/Main.java
@@ -0,0 +1,66 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int studentCount =  10;
+        List<Student> students = new ArrayList<>();
+        for(int i = 0; i < studentCount; i++){
+            students.add(new Student());
+        }
+        students.add(new LPAStudent());
+        printMoreList(students);
+
+        List<LPAStudent> lpaStudents = new ArrayList<>();
+        for(int i = 0; i < studentCount; i++){
+            lpaStudents.add(new LPAStudent());
+        }
+        printMoreList(lpaStudents);
+
+        testList(new ArrayList<>(List.of("Don","Bary","Charlie")));
+        testList(new ArrayList<>(List.of(1,2,3)));
+
+    }
+
+    public static  void printMoreList(List<? extends Student> students){
+
+        for(var student: students){
+            System.out.println(student);
+        }
+        System.out.println();
+    }
+
+    public static void testList(List<?> list){
+        for(var element: list){
+            if(element instanceof String s){
+                System.out.println("String " + s.toUpperCase());
+            } else if (element instanceof Integer i) {
+                System.out.println("Integer " + i.floatValue());
+            }
+        }
+    }
+
+    /*
+    public static void testList(List<String> list){
+        for(var element: list){
+            System.out.println("String" + element.toUpperCase());
+        }
+    }
+
+    public static void testList(List<Integer> list){
+        for(var element: list){
+            System.out.println("String" + element.floatValue());
+        }
+    }
+    */
+
+    /*
+    public static <T extends Student> void printList(List<T> students){
+        for(var student: students){
+            System.out.println(student.getYearStarted() + ": " + student);
+        }
+        System.out.println();
+    }
+    */
+}
