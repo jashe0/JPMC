816cef5b059c65681509ec39e009c2af04646766
jashe0 <120477825+jashe0@users.noreply.github.com>
2023-07-25 14:41:25 +0100

    looking at inner classes to implement a comparator

--- NestedClasses/src/Main.java
+++ NestedClasses/src/Main.java
@@ -19,5 +19,22 @@ public class Main {
             System.out.println(e);
         }
 
+        System.out.println("Store Members");
+        List<StoreEmployee> storeEmployees = new ArrayList<>(List.of(
+                new StoreEmployee(10015, "Meg",2021,"Target"),
+                new StoreEmployee(10515, "Joe", 2021, "Walmart"),
+                new StoreEmployee(10105, "Tom", 2018,"Macys"),
+                new StoreEmployee(10215,"Marty",2018,"Walmart"),
+                new StoreEmployee(10322, "Bud", 2016, "Target" )
+        ));
+
+
+        var comparator = new StoreEmployee().new StoreComparator<>();
+        storeEmployees.sort(comparator);
+
+        for(StoreEmployee e: storeEmployees){
+            System.out.println(e);
+        }
+
     }
 }
