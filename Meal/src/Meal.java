b186012ff160fe94e9349e13809836a646c898b7
jashe0 <120477825+jashe0@users.noreply.github.com>
2023-08-05 16:31:43 +0100

    complete inner class challenge

--- Meal/src/Meal.java
+++ Meal/src/Meal.java
@@ -1,7 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Meal {
 
     private double price = 5.0;
-    private Item burger;
+    private Burger burger;
     private Item drink;
     private Item side;
 
@@ -13,14 +16,14 @@ public class Meal {
 
     public Meal(double conversionRate){
         this.conversionRate = conversionRate;
-        burger = new Item("regular","burger");
+        burger = new Burger("regular");
         drink = new Item("coke","drink",1.5);
         System.out.println(drink.name);
         side = new Item("fries","side",2.0);
     }
 
     public double getTotal(){
-        double total = burger.price + drink.price + side.price;
+        double total = burger.getPrice() + drink.price + side.price;
         return Item.getPrice(total,conversionRate);
     }
 
@@ -28,6 +31,10 @@ public class Meal {
     public String toString() {
         return "%s%n%s%n%s%n%26s%.2f".formatted(burger,drink,side,
         "Total Due: ", getTotal());
+    }
+
+    public void addToppings(String... selectedToppings){
+        burger.addToppings(selectedToppings);
     }
 
     private class Item{
@@ -51,8 +58,57 @@ public class Meal {
                     getPrice(price,conversionRate));
         }
 
-        private static double getPrice(double price, double rate){
+        public static double getPrice(double price, double rate){
             return price * rate;
+        }
+    }
+
+    private class Burger extends Item{
+
+        private enum Extra {AVOCADO, BACON, CHEESE, KETCHUP, MAYO, MUSTARD, PICKLES;
+            private double getPrice(){
+                return switch (this){
+                    default -> 0;
+                    case AVOCADO -> 1.0;
+                    case BACON, CHEESE -> 1.5;
+                };
+            }
+        }
+        private List<Item> toppings = new ArrayList<>();
+
+        Burger(String name) {
+            super(name,"burger",5.0);
+        }
+
+        public double getPrice(){
+            double total = super.price;
+            for(Item topping: toppings){
+                total += topping.price;
+            }
+
+            return total;
+        }
+
+        private void addToppings(String... selectedToppings){
+            for(String selectedTopping: selectedToppings){
+                try {
+                    Extra topping = Extra.valueOf(selectedTopping.toUpperCase());
+                    toppings.add(new Item(topping.name(), "TOPPING", topping.getPrice()));
+                } catch (IllegalArgumentException ie){
+                    System.out.println(selectedTopping+ " is not a valid topping");
+                }
+
+            }
+        }
+
+        @Override
+        public String toString(){
+            StringBuilder itemized = new StringBuilder(super.toString());
+            for(Item topping : toppings){
+                itemized.append("\n");
+                itemized.append(topping);
+            }
+            return itemized.toString();
         }
     }
 }
