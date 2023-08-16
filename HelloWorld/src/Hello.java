4f285d9eaf61624215c6b47991d0bff990c3708e
jonathanashe <jonnyashe@gmail.com>
2023-08-16 18:50:46 +0100

    henlo world mac

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,5 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
+
+        System.out.println("Hello Jonny!");
         System.out.println("Hello, Tim!");
 
         boolean isAlien = false;
